5e70bcd5a79bca9e3f552a736f1736d92864f8dd
fitdizzle <markfitnessaz@gmail.com>
2022-11-13 09:06:27 -0700

    added mongo db credentials

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,11 +1,14 @@
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGO_URI || 'mongodb://54.176.23.12:27017/';
+const uri = process.env.MONGO_URI || 'attelier-service-provider:att2022!@mongodb://54.176.23.12:27017/';
 const source = process.env.DATABASE || 'attelier-product-db';
 
 module.exports = {
   getAllProducts: async (req, res) => {
     console.log('GET ALL PRODUCTS ROUTE HIT');
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     try {
       let products = await client.db(source).collection('products').find({}).toArray();
       res.json(products);
