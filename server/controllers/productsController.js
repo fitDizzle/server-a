0f8e31b0b0c9072194978392cd732dd1d227125f
fitdizzle <markfitnessaz@gmail.com>
2022-11-13 09:52:33 -0700

    yet again

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGO_URI || 'mongodb://54.176.23.12:super:super123@27017/';
+var username = encodeURIComponent("super");
+var password = encodeURIComponent("super123");
+const uri = process.env.MONGO_URI || `mongodb://${username}:${password}@54.176.23.12:27017/`;
 const source = process.env.DATABASE || 'attelier-product-db';
 
 module.exports = {
@@ -7,7 +9,8 @@ module.exports = {
     console.log('GET ALL PRODUCTS ROUTE HIT');
     const client = new MongoClient(uri, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      ssl: true
     });
     try {
       let products = await client.db(source).collection('products').find({}).toArray();
